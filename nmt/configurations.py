67ec15468f590eb6caad1d6dde91f4adcc142425
Toan Nguyen <toannq177@gmail.com>
2019-04-13 09:10:31 -0400

    [r] minor

--- nmt/configurations.py
+++ nmt/configurations.py
@@ -7,11 +7,12 @@ import nmt.all_constants as ac
 """You can add your own configuration function to this file and select
 it using `--proto function_name`."""
 
+
 def base_config():
     config = {}
 
     ### Locations of input/output files
-    
+
     # The name of the model
     config['model_name']        = 'model_name'
 
@@ -22,7 +23,7 @@ def base_config():
 
     # Directory to read input files from
     config['data_dir']          = './nmt/data/{}'.format(config['model_name'])
-    
+
     # Directory to save models and other outputs in
     config['save_to']           = './nmt/saved_models/{}'.format(config['model_name'])
 
@@ -30,10 +31,10 @@ def base_config():
     config['log_file']          = './nmt/DEBUG.log'
 
     ### Model options
-    
+
     # Filter out sentences longer than this (minus one for bos/eos)
     config['max_train_length']  = 1000
-    
+
     # Vocabulary sizes
     config['src_vocab_size']    = 0
     config['trg_vocab_size']    = 0
@@ -65,17 +66,17 @@ def base_config():
     config['norm_in']           = True
 
     ### Dropout/smoothing options
-    
+
     config['dropout']           = 0.3
     config['word_dropout']      = 0.1
     config['label_smoothing']   = 0.1
-    
+
     ### Training options
-    
+
     config['batch_sort_src']    = True
     config['batch_size']        = 4096
     config['weight_init_type']  = ac.XAVIER_NORMAL
-    config['normalize_loss']    = ac.LOSS_TOK # don't see any difference between loss_batch and loss_tok
+    config['normalize_loss']    = ac.LOSS_TOK
 
     # Hyperparameters for Adam optimizer
     config['beta1']             = 0.9
@@ -93,27 +94,25 @@ def base_config():
 
     # Gradient clipping
     config['grad_clip']         = 1.0 # if no clip, just set it to some big value like 1e9
-    
-    config['reload']            = True
 
     ### Validation/stopping options
-    
+
     config['max_epochs']        = 100
     config['validate_freq']     = 1.0 # eval every [this many] epochs
     config['val_per_epoch']     = True # if this true, we eval after every [validate_freq] epochs, otherwise by num of batches
     config['val_by_bleu']       = True
-    
+
     # Undo BPE segmentation when validating
     config['restore_segments']  = True
-    
+
     # How many of the best models to save
     config['n_best']            = 1
-    
+
     config['val_trans_out']     = os.path.join(config['save_to'], 'validation_trans.txt')
     config['val_beam_out']      = os.path.join(config['save_to'], 'beam_trans.txt')
-    
+
     ### Decoding options
-    
+
     config['beam_size']         = 4
     config['beam_alpha']        = 0.6
 
